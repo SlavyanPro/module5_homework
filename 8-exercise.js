1d0299fb814862de9de16b6fde098494ca243170
SlavyanPro <95080055+SlavyanPro@users.noreply.github.com>
2023-11-30 11:36:45 +0600

    8-exercise.js

--- 8-exercise.js
+++ 8-exercise.js
@@ -1,10 +1,20 @@
-let things = new Map();
-things.set("table", "brown");
-things.set("TV", "black");
-things.set("piano", "white");
-things.set("fridge", "white");
+let box = new Map();
+box.set("Box_1", "red");
+box.set("Box_2", "orange");
+box.set("Box_3", "yellow");
+box.set("Box_4", "green");
+box.set("Box_5", "blue");
+box.set("Box_6", "purple");
+box.set("Box_7", "pink");
+box.set("Box_8", "white");
   
-for (let things_s of things.keys()) {
-    console.log(things_s + " is " + things.get(things_s));
-  }
+for (let boxes of box.keys()) {
+  console.log(boxes + " - X");
+}
+for (let boxes of box.values()) {
+  console.log(boxes + " - Y");
+}
+for (let boxes of box.keys()) {
+console.log(boxes + " - X , " + box.get(boxes) + " - Y" );
+}
   
